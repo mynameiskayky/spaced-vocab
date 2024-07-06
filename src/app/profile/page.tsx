dce0bbb2acfa9f2e56e41327348117d673ef02c0
nynameiskayky <kaykyvieiraa@hotmail.com>
2024-07-06 01:16:14 -0300

    feat: add skeleton when loading profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -10,6 +10,7 @@ import {
   CardDescription,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface User {
   id: number;
@@ -19,6 +20,7 @@ interface User {
 export default function Profile() {
   const router = useRouter();
   const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const token = sessionStorage.getItem("token");
@@ -27,12 +29,25 @@ export default function Profile() {
       return;
     }
 
-    const user = JSON.parse(atob(token.split(".")[1]));
-    setUser(user);
+    setTimeout(() => {
+      const user = JSON.parse(atob(token.split(".")[1]));
+      setUser(user);
+      setLoading(false);
+    }, 1500);
   }, [router]);
 
-  if (!user) {
-    return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <div className="flex min-h-[100dvh] items-center justify-center bg-background">
+        <div className="w-full max-w-md space-y-4">
+          <Skeleton className="h-8 w-3/4 rounded-xl" />
+          <Skeleton className="h-6 w-1/2 rounded-xl" />
+          <Skeleton className="h-6 w-full rounded-xl" />
+          <Skeleton className="h-6 w-full rounded-xl" />
+          <Skeleton className="h-10 w-full rounded-xl" />
+        </div>
+      </div>
+    );
   }
 
   const handleLogout = () => {
